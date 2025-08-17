d8d0dd3b7a79a2191f853da3957418f43e58dc17
AndreasHM02 <andreashm@live.no>
2025-08-17 22:43:41 +0200

    Changed apperance of norwegian cv

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+
+function show(elementId) {
+
+  var element = document.getElementById(elementId);
+  console.log("Toggling element with ID:", elementId);
+  if ( element.style.display === "none" ) {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
   }
 }
